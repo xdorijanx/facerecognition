1494cbca6d92d4c4a88a96490339eb8ebd52afbb
xdorijanx <dorijan11@gmail.com>
2019-08-12 22:20:51 +0200

    Add rank component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
 import './App.css';
 
+
+const particlesOptions = {
+ particles: {
+  number: {
+    value: 300,
+    density: {
+      enable: true,
+      
+    }
+  }
+ }
+}
 function App() {
   return (
     <div className="App">
+          <Particles  className='particles'
+                params={particlesOptions} />
       <Navigation />
      <Logo />
+     <Rank />
     <ImageLinkForm />
    { /*  <FaceRecognition /> */}
     </div>
