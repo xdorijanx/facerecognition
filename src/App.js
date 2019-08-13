0aed7fc5809b838b39615ccb291ba23ed034415f
xdorijanx <dorijan11@gmail.com>
2019-08-14 00:05:38 +0200

    Add register form and routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
@@ -29,7 +30,8 @@ class App extends Component {
     input: "",
     imageUrl: "",
     box: {},
-    route: "signin"
+    route: "signin",
+    isSignedIn: false
   };
   calculateFaceLocation = data => {
     const clarifaiFace =
@@ -65,20 +67,25 @@ class App extends Component {
       .catch(err => console.log("[ERROR]:", err));
   };
 
-  onRouteChange = (route) => {
+  onRouteChange = route => {
+
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({
       route: route
-    })
-  }
+    });
+  };
 
   render() {
+    const { isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        {this.state.route === "signin" ? (
-          <Signin onRouteChange={this.onRouteChange}/>
-        ) : (
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === "home" ? (
           <div>
             <Logo />
             <Rank />
@@ -87,10 +94,14 @@ class App extends Component {
               onButtonSubmit={this.onButtonSubmit}
             />
             <FaceRecognition
-              imageUrl={this.state.imageUrl}
-              box={this.state.box}
+              imageUrl={imageUrl}
+              box={box}
             />
           </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
         )}
       </div>
     );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+
 const Signin = ({onRouteChange}) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
       <main className="pa4 black-80">
-        <form className="measure ">
+        <div className="measure ">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Email
               </label>
               <input
@@ -19,7 +20,7 @@ const Signin = ({onRouteChange}) => {
               />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
                 Password
               </label>
               <input
@@ -42,12 +43,12 @@ const Signin = ({onRouteChange}) => {
             />
           </div>
           <div className="lh-copy mt3">
-            <a href="#0" className="f6 link dim black db">
+            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">
               Register
-            </a>
+            </p>
           
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
